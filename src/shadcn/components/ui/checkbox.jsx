2dbd963dad053eb45b91d18636de3a3de8eaed5a
pranaaavj <jpvelloor@gmail.com>
2025-04-08 02:15:01 +0530

    test commit

--- src/shadcn/components/ui/checkbox.jsx
+++ src/shadcn/components/ui/checkbox.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { CheckIcon } from '@radix-ui/react-icons';
-/* eslint-disable */
 import { cn } from '@/shadcn/lib/utils';
 
 const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
